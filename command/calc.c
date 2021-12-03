5d9537129e5030e14aa909497465703ae6fc9c5d
pn20120162 <hNU7BiSRI8>
2021-12-03 10:02:22 +0800

    格式化

--- command/calc.c
+++ command/calc.c
@@ -1,7 +1,8 @@
 /***************************calculator*********************************/
-#include<stdio.h>
+#include <stdio.h>
 #define CALC_NUM(x) (x - 0)
-void main(){
+void main()
+{
 	// 有bug
 	printf("***************************************************\n");
 	printf("*               Calculator                        *\n");
@@ -10,34 +11,37 @@ void main(){
 	printf("*              Enter e to quit                    *\n");
 	printf("*                                                 *\n");
 	printf("***************************************************\n\n");
-	while(1){	
-	char result;
+	while (1)
+	{
+		char result;
 
-	char bufr[128];
-	read(0, bufr, 128);
+		char bufr[128];
+		read(0, bufr, 128);
 
-	switch(bufr[1]){
+		switch (bufr[1])
+		{
+		case '+':
+			result = CALC_NUM(bufr[0]) + CALC_NUM(bufr[2]);
+			printf("jia fa \n");
+			break;
+		case '-':
+			result = CALC_NUM(bufr[0]) - CALC_NUM(bufr[2]);
+			printf("jian fa \n");
+			break;
 
-    case '+':result=CALC_NUM(bufr[0])+CALC_NUM(bufr[2]);
-	printf("jia fa \n");
-	break;
-	case '-':result=CALC_NUM(bufr[0])-CALC_NUM(bufr[2]);
-	printf("jian fa \n");
-	break;
+		case '*':
+			result = CALC_NUM(bufr[0]) * CALC_NUM(bufr[2]);
+			printf("cheng fa \n");
+			break;
 
-	case '*':result=CALC_NUM(bufr[0])*CALC_NUM(bufr[2]);
-	printf("cheng fa \n");
-	break;
-
-	case '/':result=CALC_NUM(bufr[0])/CALC_NUM(bufr[2]);
-	printf("chu fa \n");
-	break;
-
+		case '/':
+			result = CALC_NUM(bufr[0]) / CALC_NUM(bufr[2]);
+			printf("chu fa \n");
+			break;
+		}
+		if (bufr[0] == 'e')
+			break;
+		else
+			printf("%d %c %d = %d\n", CALC_NUM(bufr[0]), bufr[1], CALC_NUM(bufr[2]), result);
 	}
-	if(bufr[0]=='e')
-		break;
-	else
-		printf("%d %c %d = %d\n",CALC_NUM(bufr[0]),bufr[1],CALC_NUM(bufr[2]),result);
-	}
-
 }
